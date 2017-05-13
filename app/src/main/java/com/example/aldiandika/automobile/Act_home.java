42fbb05883223ddf549d4e3f67a69eadacc8cd48
aldiandika <aldi.andika@gmail.com>
2017-05-13 19:17:41 +0700

    done

--- app/src/main/java/com/example/aldiandika/automobile/Act_home.java
+++ app/src/main/java/com/example/aldiandika/automobile/Act_home.java
@@ -54,8 +54,11 @@ public class Act_home extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        super.onBackPressed();
-        finish();
+//        super.onBackPressed();
+        Intent startMain = new Intent(Intent.ACTION_MAIN);
+        startMain.addCategory(Intent.CATEGORY_HOME);
+        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(startMain);
     }
 
     @Override
