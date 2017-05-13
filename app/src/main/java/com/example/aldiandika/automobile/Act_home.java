2f1ef55b5f88a3304ac9a6eb32d4955a24a81631
aldiandika <aldi.andika@gmail.com>
2017-05-13 16:25:51 +0700

    main sisa -get Directions -toolbar + immersive mode

--- app/src/main/java/com/example/aldiandika/automobile/Act_home.java
+++ app/src/main/java/com/example/aldiandika/automobile/Act_home.java
@@ -3,17 +3,22 @@ package com.example.aldiandika.automobile;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 public class Act_home extends AppCompatActivity {
 
     View decorView;
-    ImageView btn_repair;
-    ListView list1;
+    LinearLayout btn_repair;
+    RecyclerView recyclerViewH;
+    RecyclerView.LayoutManager layoutManagerH;
+    RecyclerView.Adapter adapterH;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +27,16 @@ public class Act_home extends AppCompatActivity {
 
         decorView = getWindow().getDecorView();
 
-        btn_repair = (ImageView) findViewById(R.id.btn_repair);
-        list1 = (ListView) findViewById(R.id.list1);
+        btn_repair = (LinearLayout) findViewById(R.id.btn_repair);
+
+        recyclerViewH = (RecyclerView) findViewById(R.id.tampil);
+
+        layoutManagerH = new LinearLayoutManager(this);
+        recyclerViewH.setLayoutManager(layoutManagerH);
+
+        adapterH = new RecyclerAdapterH();
+        recyclerViewH.setAdapter(adapterH);
+
 
     }
 
