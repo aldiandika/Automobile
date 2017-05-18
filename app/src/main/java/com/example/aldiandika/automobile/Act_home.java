ee86ff8620a8c9363ca1839e9db514b888d5016f
aldiandika <aldi.andika@gmail.com>
2017-05-19 01:25:14 +0700

    Sisa - Main Act java - Repair - Market - Garage

--- app/src/main/java/com/example/aldiandika/automobile/Act_home.java
+++ app/src/main/java/com/example/aldiandika/automobile/Act_home.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -15,10 +16,10 @@ import android.widget.ListView;
 public class Act_home extends AppCompatActivity {
 
     View decorView;
-    LinearLayout btn_repair;
-    RecyclerView recyclerViewH;
-    RecyclerView.LayoutManager layoutManagerH;
-    RecyclerView.Adapter adapterH;
+    ImageButton btn_repair;
+//    RecyclerView recyclerViewH;
+//    RecyclerView.LayoutManager layoutManagerH;
+//    RecyclerView.Adapter adapterH;
     ImageView direk;
 
     @Override
@@ -28,16 +29,16 @@ public class Act_home extends AppCompatActivity {
 
         decorView = getWindow().getDecorView();
 
-        btn_repair = (LinearLayout) findViewById(R.id.btn_repair);
-        direk = (ImageView) findViewById(R.id.direk);
+//        btn_repair = (ImageButton) findViewById(R.id.btn_repair);
+//        direk = (ImageView) findViewById(R.id.direk);
 
-        recyclerViewH = (RecyclerView) findViewById(R.id.tampil);
-
-        layoutManagerH = new LinearLayoutManager(this);
-        recyclerViewH.setLayoutManager(layoutManagerH);
-
-        adapterH = new RecyclerAdapterH();
-        recyclerViewH.setAdapter(adapterH);
+//        recyclerViewH = (RecyclerView) findViewById(R.id.tampil);
+//
+//        layoutManagerH = new LinearLayoutManager(this);
+//        recyclerViewH.setLayoutManager(layoutManagerH);
+//
+//        adapterH = new RecyclerAdapterH();
+//        recyclerViewH.setAdapter(adapterH);
 
 
     }
@@ -66,8 +67,7 @@ public class Act_home extends AppCompatActivity {
         super.onWindowFocusChanged(hasFocus);
         if (hasFocus) {
             decorView.setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                             | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                             | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
                             | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
